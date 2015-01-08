a42eaf2e7800486fe14b039346e622f3a6b5c7d9
Julia Baritz <jbaritz@gmail.com>
2015-01-08 15:02:13 -0600

    add images

--- public/main.js
+++ public/main.js
@@ -1,1 +1,1 @@
-frontpageAlert = _.template("<div class='col-md-1'></div><div class='col-md-4 bulletin'><h3><%=dogname %></h3> Found at: <%=location %></div><div class='col-md-1'></div>") //this is a template, call it to build bulletins
+frontpageAlert = _.template("<div class='col-md-1'></div><div class='col-md-4 bulletin'><h3><%=dogname %></h3><img src= <%= image %> > Found at: <%=location %></div><div class='col-md-1'></div>") //this is a template, call it to build bulletins
