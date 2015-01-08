e0968081d59460a4686b93e218dcd3dc58d3b3b5
Greg Mikeska <gmikeska07@gmail.com>
2015-01-08 10:58:20 -0600

    make it better

---
+++ server.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+# require 'sinatra/reloader'
+require 'rest-client'
+require 'json'
+require 'bcrypt'
+require_relative 'config/environments.rb'
+
+
+class FindMyPet::Server < Sinatra::Application
+	configure do
+	    enable :sessions
+	  end
+
+	before do
+		if session['user_id']
+			user_id = session['user_id']
+		end
+	 end
+	# #
+	# This is our only html view...
+	#
+	get '/' do
+	  	erb :index
+	end
+end
