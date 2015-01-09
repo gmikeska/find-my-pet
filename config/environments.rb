424be9aebf41ad2b7799a55ff456b1652e4bae84
Julia Baritz <jbaritz@gmail.com>
2015-01-09 11:22:00 -0600

    change environments

--- config/environments.rb
+++ config/environments.rb
@@ -1,8 +1,9 @@
 require 'active_record'
 require 'active_record_tasks'
 require_relative '../lib/models.rb' # the path to your application file
+require_relative '../lib/FindMyPet.rb'
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
-  :database => 'dfuf4gtvlkndlc'
+  :database => 'findmypet'
 )
