48c2b54261c01716d95bedb664f9c92cd946dd3b
Greg Mikeska <gmikeska07@gmail.com>
2015-01-12 11:59:34 -0600

    add some cat images to bknd

--- public/main.js
+++ public/main.js
@@ -37,7 +37,11 @@ var backgrounds = ["http://peacelovefoster.files.wordpress.com/2012/11/09.jpg",
 		"http://www.animalcareclinicslo.com/wp-content/uploads/2011/03/dog-and-tennis-ball.jpg",
 		"http://www.petmeds.org/wp-content/uploads/2012/09/26901192_Abir_with_TB.jpg",
 		"http://www.imgion.com/images/01/White-Cute-Puppy-.jpg",
-		"http://animalfair.com/wp-content/uploads/2013/09/macexplorer.com-puppy-dog-26.jpg"]
+		"http://animalfair.com/wp-content/uploads/2013/09/macexplorer.com-puppy-dog-26.jpg",
+		"http://scienceblogs.com/gregladen/files/2012/12/Beautifull-cat-cats-14749885-1600-1200.jpg",
+		"http://eofdreams.com/data_images/dreams/cat/cat-05.jpg",
+		"http://upload.wikimedia.org/wikipedia/commons/9/95/CatVibrissaeFullFace.JPG",
+		"http://i2.mirror.co.uk/incoming/article99763.ece/alternates/s2197/a-bengal-cat-935179217.jpg"]
 $(document).ready(function(){
 	console.log('running')
 	i = Math.floor(Math.random() * (backgrounds.length));
